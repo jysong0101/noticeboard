67afb7146c9fccb76734ff3c6dcc7fb6885312bb
jysong0101 <juyeop0101@khu.ac.kr>
2022-11-24 21:56:24 +0900

    Initial commit

---
+++ db/db_conn.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'asns7873',
+    database: 'notice'
+}
---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const mysql = require("mysql")
+const bodyParser= require('body-parser')
+
+const db_config = require("./db/db_conn")
+
+const app = express()
+const connection = mysql.createConnection(db_config)
+
+const PORT = 3456
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true})) 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/post', (req, res) => {
+    res.sendFile(__dirname + "/post.html")
+})
+
+app.post('/post', (req, res) => {
+    title = req.body.title
+    content = req.body.content
+
+    connection.query('INSERT INTO notice (notice_title, notice_content) VALUES (?, ?)', [title, content], (err, rows) =>{
+        if (err){
+            res.send("db 오류입니다. 다시 시도하세요.")
+        }
+        else{
+            res.redirect('/')
+        }
+    })
+})
+
+app.get('/retrieve', (req, res)=>{
+    res.sendFile(__dirname+"/retrieve.html")
+})
+
+app.get('/api/retrieve', (req, res) => {
+    connection.query('select notice_title, notice_content from notice', (err, rows) =>{
+        if (err){
+            res.send("db 오류입니다. 다시 시도하세요.")
+        }
+        else{
+            res.json(rows)
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("server on")
+})
