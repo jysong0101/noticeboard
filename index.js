f507bad3b38cbf37caea058b9154fa96010dcbae
jysong0101 <juyeop0101@khu.ac.kr>
2022-11-28 20:23:51 +0900

    본문확인 줄 추가

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // 구현 시작
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/main.html')
 })
@@ -40,6 +38,21 @@ app.get('/fix', (req, res) => {
     res.sendFile(__dirname + "/fix.html")
 })
 
+app.post('/fix', (req, res) => {
+    title = req.body.title
+    content = req.body.content
+    username = req.body.username
+
+    connection.query('UPDATE tab1, tab2 SET tab1.column1 = value1, tab1.column2 = value2 WHERE tab1.id = tab2.id;', [title, content, username], (err, rows) =>{
+        if (err){
+            res.send("db 오류입니다.")
+        }
+        else{
+            res.redirect('/')
+        }
+    })
+})
+
 app.post('/post', (req, res) => {
     title = req.body.title
     content = req.body.content
@@ -47,7 +60,7 @@ app.post('/post', (req, res) => {
 
     connection.query('INSERT INTO notice (notice_title, notice_content, notice_username) VALUES (?, ?, ?)', [title, content, username], (err, rows) =>{
         if (err){
-            res.send("db 오류입니다. 다시 시도하세요.")
+            res.send("db 오류입니다.")
         }
         else{
             res.redirect('/')
